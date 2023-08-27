7842f56828d9f745c843f6dc76aac06bf4a10e8c
Sanjeev kumar <sanjeevkumarrao@gmail.com>
2023-08-27 21:16:00 +1000

    removing unwanted file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sequelize = require("sequelize");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const dbusers = require('./db')
+// const dbusers = require('./db')
 const app = express();
 const db = require("./Models");
 const userRoutes = require("./Routes/userRoutes");
@@ -14,6 +14,25 @@ const PORT = process.env.PORT || 4000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
